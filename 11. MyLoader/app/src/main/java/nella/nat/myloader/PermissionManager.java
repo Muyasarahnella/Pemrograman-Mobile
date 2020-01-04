b6bf84f846f8678c9c4ecbd8b9a082c8076daabc
MuyasarahNella <nellafawn@gmail.com>
2020-01-04 10:13:59 +0700

    upload

---
+++ 11. MyLoader/app/src/main/java/nella/nat/myloader/PermissionManager.java
@@ -0,0 +1,22 @@
+package nella.nat.myloader;
+
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+
+public class PermissionManager {
+    public static void check(Activity activity, String permission, int requestCode){
+        if(ActivityCompat.checkSelfPermission(activity, permission) !=
+                PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(activity, new String[]{permission},
+                    requestCode);
+        }
+    }
+
+    public static boolean isGranted(Activity activity, String permission){
+        return (ActivityCompat.checkSelfPermission(activity, permission) ==
+                PackageManager.PERMISSION_DENIED);
+    }
+}
